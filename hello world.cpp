e4e2664fa201c18802fe03018959ee55d94b4907
Henrique André <henrique_andre10@hotmail.com>
2022-12-03 08:02:44 -0300

    hello world complete

--- hello world.cpp
+++ hello world.cpp
@@ -3,8 +3,23 @@
 using namespace std;
 
 int main (){
-
     cout << "hello world" << endl;
+
+    int test;
+    string nome;
+
+    cout << "type a number: ";
+    cin >> test;
+    cout << "number typed:" << test << "\n";
+    cout << "\ntype a string: ";
+    cin >> nome;
+    cout << "string typed: " << nome << "\n";
+
+    // ------------
+
+    (test==2) ? nome="lucky day\n" : nome="bad lucky\n";
+
+    cout << nome;
 
     return 0;
 }
