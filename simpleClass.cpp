d459c8e8b2352e14165d0b1acd4b641643eca242
Henrique André <henrique_andre10@hotmail.com>
2022-12-06 07:06:39 -0300

    implement simpleClass function

---
+++ simpleClass.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class Drugs {
+
+    private:
+    int cost=0;
+
+    public:
+    int qnt_inventory;
+    string name;
+
+    void incr_cost();
+    void decr_cost();
+    void value();
+};
+
+void Drugs::incr_cost(){
+    this->cost++;
+}
+
+void Drugs::decr_cost(){
+    this->cost--;
+}
+
+void Drugs::value(){
+    cout << this->cost << "\n";
+}
+
+int main() {
+
+    Drugs *medicine = new Drugs; 
+
+    medicine->value();
+    medicine->incr_cost();
+    medicine->value();
+    medicine->decr_cost();
+    medicine->value();
+
+    medicine->name = "Dorflex";
+    medicine->qnt_inventory = 5;
+
+    cout << "name: " << medicine->name << "\n";
+    cout << "inventory: " << medicine->qnt_inventory << "\n";
+    medicine->incr_cost();
+    medicine->value();
+
+    return 0;
+}
