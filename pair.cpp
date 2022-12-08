d12ba15104641cd30e12ab20dbaa127260b08410
Henrique André <henrique_andre10@hotmail.com>
2022-12-08 08:19:51 -0300

    using utility library to make pairs

---
+++ pair.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main (){
+
+    pair<int, string> test;
+
+    test.first = 100;
+    test.second = "Bruno";
+    cout << test.first << " - " << test.second << endl;
+
+    test=make_pair(10, "Aethos");
+    cout << test.first << " - " << test.second << endl;
+
+    vector<pair<int, string>> test2;
+
+    test2.push_back(make_pair(1, "Abbha"));
+    test2.push_back(make_pair(2, "Beta"));
+    test2.push_back(make_pair(3, "Cetrhos"));
+
+    for (auto i:test2){
+        cout << i.first << " - " << i.second << endl;
+    }
+
+    return 0;
+}
